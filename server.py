45f0f57c40d4592c0b5e1fde9b5428079ac435bb
Zachkery Rodriguez <39169752+ZVR999@users.noreply.github.com>
2018-08-19 14:59:12 -0500

    Templates/Completed routes
    Added remaining jinja2 templating needed display required query/result information for this assignment.
    Added 3 variables into the render_template()s on the server.py to display an error of duplication of a email already in the database, another to show success of the newly added email, and a for loop showing all of the emails currently in the database.
    Added 3 queries to server.py allow all of the above to work with no issues.

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, redirect, redirect
+from flask import Flask, render_template, redirect, redirect, request
 from mysqlconnection import MySQLConnector
+from datetime import datetime
 
 app = Flask(__name__)
 mysql = MySQLConnector(app, 'emaildb')
@@ -8,8 +9,32 @@ mysql = MySQLConnector(app, 'emaildb')
 def index():
     return render_template('index.html')
 
+@app.route('/check', methods=['POST'])
+def check():
+    data = {
+       'email': request.form['email']
+    }
+    query = 'SELECT * FROM emails WHERE emails.email = :email'
+    print type(mysql.query_db(query,data))
+    if mysql.query_db(query,data) == []:
+        query = 'INSERT INTO emails(email,updated_at,created_at) VALUES (:email,now(),now())'
+        mysql.query_db(query,data)
+    else:
+        invalid = ''
+        invalid += '<div>Email is not valid!</div>'
+        return render_template('index.html', fail=invalid)
+    return redirect('/success')
+
 @app.route('/success')
 def success():
-    return render_template('success.html')
+    query = 'SELECT * FROM emails;'
+    emailString = ''
+    emails = mysql.query_db(query)
+    for email in emails:
+        emailString += '<p>'+email['email']+'</p>'
+    print emails[-1]['email']
+    resultString = ''
+    resultString += '<div>The email address you entered ('+emails[-1]['email']+') is a VALID email address! Thank you!</div>'
+    return render_template('success.html', entered=emailString, result=resultString)
 
 app.run(debug=True)
